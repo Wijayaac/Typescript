b380479645f3224723f1173b2121d168d17bd003
Wijaya <wijayaac25@gmail.com>
2020-12-04 11:29:49 +0800

    overriding super

--- index.ts
+++ index.ts
@@ -3,18 +3,25 @@ export class Animal {
     age: number;
     legs: number;
     name: string;
+    lives = 1;
 
     constructor(age:number,legs:number,name:string) {
         this.age = age;
         this.legs = legs;
         this.name = name;
     }
+    pus(): string {
+        return 'NGEENGG NGENNGG';
+    }
 }
 export class Cat extends Animal{
+    lives = 9;
     constructor(data :{age:number,legs:number,name:string}) {
         super(data.age, data.legs, data.name);
     }
     pus(): string {
+        super.pus();
+        super.lives;
         return 'PUSS MEONG ! PUSS MEONG!';
     }
 }
@@ -23,6 +30,7 @@ export class Dog implements Animal{
     age:number;
     legs: number;
     name: string;
+    lives: 1;
     gugug():string{
         return 'GUG! GUGUG! GUGUGUG!'
     }
