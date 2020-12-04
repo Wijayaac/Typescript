2c65e684354cd9c42c9865df8522a3381decab9f
Wijaya <wijayaac25@gmail.com>
2020-12-04 13:10:44 +0800

    protected encapsulation

--- index.ts
+++ index.ts
@@ -1,23 +1,34 @@
 export class Animal {
-    public age : number,
-    private legs : number;
-    name :string;
+  protected age: number;
+  legs: number;
+  name: string;
 
-    constructor(age:number, legs: number, name:string) {
-        this.age = age;
-        this.legs = legs;
-        this.name = name;
-    }
+  constructor(age: number, legs: number, name: string) {
+    this.age = age;
+    this.legs = legs;
+    this.name = name;
+  }
+}
+// child
+export class Cat extends Animal {
+  constructor(data: { age: number; legs: number; name: string }) {
+    super(data.age, data.legs, data.name);
+  }
+  birthday(): number {
+    return this.age + 1;
+  }
 }
 
-export class Cat extends Animal{
-    constructor(data : {age :number, legs: number, name:string}){
-        super(data.age,data.legs,data.name);
-    }
+// shape
+// hard to implement other class that have private or protected value.
+export class Dog implements Animal {
+  public age: number;
+  legs: number;
+  name: string;
 }
 
-const firstCat = new Cat({age:24,legs:4,name:'Catty'})
-firstCat.age;
+const firstCat = new Cat({ age: 24, legs: 4, name: "Catty" });
+firstCat.birthday;
 firstCat.name;
 //firstCat.legs;
 //return legs cannot acces outside animal class
