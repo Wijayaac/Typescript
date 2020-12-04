66b4588fca21760fc6f14f2e1ed942eb93205b81
Wijaya <wijayaac25@gmail.com>
2020-12-04 09:55:43 +0800

    method on TS

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ export class Message{
         this.message = message;
         this.isSent = false;
     }
+    previewMessage():string {
+        return this.message.slice(0 , 10).concat('...');
+    }
 }
 
-const message = new Message('Say Hello', 'Hello Wijaya')
+const message = new Message('Say Hello', 'Hello Wijaya how are you today ?');
+
+message.previewMessage();
