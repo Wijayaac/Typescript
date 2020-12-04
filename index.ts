2f69dd41fbbad3bf45938d0ff4f07b374da01c6d
Wijaya <wijayaac25@gmail.com>
2020-12-04 11:25:04 +0800

    comparison implement and extends

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
 // base parent class
 export class Animal {
     age: number;
-    leg: number;
+    legs: number;
     name: string;
 
-    constructor(age:number,leg:number,name:string) {
+    constructor(age:number,legs:number,name:string) {
         this.age = age;
-        this.leg = leg;
+        this.legs = legs;
         this.name = name;
+    }
+}
+export class Cat extends Animal{
+    constructor(data :{age:number,legs:number,name:string}) {
+        super(data.age, data.legs, data.name);
+    }
+    pus(): string {
+        return 'PUSS MEONG ! PUSS MEONG!';
     }
 }
 // Derived / Child Class / Same Shape
 export class Dog implements Animal{
     age:number;
-    leg: number;
+    legs: number;
     name: string;
     gugug():string{
         return 'GUG! GUGUG! GUGUGUG!'
     }
 }
-    
+
+const firstDog = new Dog();
+const firstCat = new Cat({age: 25, legs:4, name: 'Catty'});
+
+
+console.log(firstDog instanceof Animal);
+//return false 
+console.log(firstCat instanceof Animal);
+//return true    
